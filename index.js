251a7e0602aefbf24e2e4b2592c54551b60c6729
Manuel Moran <manumoran158@gmail.com>
2022-07-28 23:16:18 -0300

    js

---
+++ index.js
@@ -0,0 +1,17 @@
+let formulario = document.getElementById("formulario");
+formulario.addEventListener("submit", validarFormulario);
+
+let correo = document.getElementById("correo");
+
+
+function validarFormulario(e){
+    e.preventDefault();
+
+    //correo
+    console.log(e.target.children[0].children[1]);
+    if(e.target.children[0].value.includes("1")){
+       let mensaje = document.createElement("div");
+       mensaje.innerHTML = "no es un correo";
+       document.body.append(mensaje);
+    }
+}
