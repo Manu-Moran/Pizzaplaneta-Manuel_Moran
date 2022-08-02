4a64e10f8aea748569192860f8d60d2fbaf26be0
Manuel Moran <manumoran158@gmail.com>
2022-08-02 07:28:53 -0300

    pato x2

--- index.js
+++ index.js
@@ -20,6 +20,24 @@ const btnSwitch = document.querySelector('#switch');
 
 btnSwitch.addEventListener('click', () =>{
     document.body.classList.toggle('dark');
-    btnSwitch.classList.toggle('active')
+    btnSwitch.classList.toggle('active');
+
+
+    /* guardar con storage */
+
+    if(document.body.classList.contains('dark')){
+        localStorage.setItem('dark-mode', 'true');
+    } else {
+        localStorage.setItem('dark-mode', 'false');
+    }
     }
 )
+
+/* obtener el actual */
+if(localStorage.getItem('dark-mode') === 'true'){
+    document.body.classList.add('dark');
+    btnSwitch.classList.add('active');
+} else{
+    document.body.classList.remove('dark');
+    btnSwitch.classList.remove('active')
+}
