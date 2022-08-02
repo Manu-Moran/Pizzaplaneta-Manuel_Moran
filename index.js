cab820594c24de1ec9b1d030e1157acd6408e0a9
Manuel Moran <manumoran158@gmail.com>
2022-08-02 07:05:04 -0300

    pato

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
-let formulario = document.getElementById("formulario");
-formulario.addEventListener("submit", validarFormulario);
+// let formulario = document.getElementById("formulario");
+// formulario.addEventListener("submit", validarFormulario);
 
-let correo = document.getElementById("correo");
+// let correo = document.getElementById("correo");
 
 
-function validarFormulario(e){
-    e.preventDefault();
+// function validarFormulario(e){
+//     e.preventDefault();
 
-    //correo
-    console.log(e.target.children[0].children[1]);
-    if(e.target.children[0].value.includes("1")){
-       let mensaje = document.createElement("div");
-       mensaje.innerHTML = "no es un correo";
-       document.body.append(mensaje);
+//     //correo
+//     console.log(e.target.children[0].children[1]);
+//     if(e.target.children[0].value.includes("1")){
+//        let mensaje = document.createElement("div");
+//        mensaje.innerHTML = "no es un correo";
+//        document.body.append(mensaje);
+//     }
+// }
+
+const btnSwitch = document.querySelector('#switch');
+
+btnSwitch.addEventListener('click', () =>{
+    document.body.classList.toggle('dark');
+    btnSwitch.classList.toggle('active')
     }
-}
+)
